75a6eeb4ce376aabf7c66b55baf994e193667c52
Deepika Malini Rajasekar <deepirajsekar03@gmail.com>
2023-01-27 15:08:15 +0200

    initial setup

---
+++ src/main/java/com/assingment/hscitybikesjourney/HscitybikesjourneyApplication.java
@@ -0,0 +1,13 @@
+package com.assingment.hscitybikesjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HscitybikesjourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HscitybikesjourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assingment/hscitybikesjourney/HscitybikesjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assingment.hscitybikesjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HscitybikesjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
