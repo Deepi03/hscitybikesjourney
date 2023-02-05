710ea3294d5b8a87383eaadf0996bb74065af8b8
Deepika Malini Rajasekar <deepirajsekar03@gmail.com>
2023-02-05 11:20:50 +0200

    add id property to Journey document

--- src/main/java/com/assingment/hscitybikesjourney/dto/Journey.java
+++ src/main/java/com/assingment/hscitybikesjourney/dto/Journey.java
@@ -1,24 +1,27 @@
 package com.assingment.hscitybikesjourney.dto;
 
+import javax.persistence.Id;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Document(collection = "journey")
+@Data
+@RequiredArgsConstructor
 public class Journey {
-
-    private String departureTime;
-    private String returnTime;
-    private String departureStationId;
-    private String departureStation;
-    private String returnStationId;
-    private String returnStation;
-    private Long coveredDistance;
-    private Long duration;
+    @Id
+    private String id;
+    private final String departureTime;
+    private final String returnTime;
+    private final String departureStationId;
+    private final String departureStation;
+    private final String returnStationId;
+    private final String returnStation;
+    private final Long coveredDistance;
+    private final Long duration;
 
 }
